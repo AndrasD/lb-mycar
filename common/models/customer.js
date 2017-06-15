e85c27d7293f6547732b052089cc514a4b5355fc
AndrasD <andras.dallos@gmail.com>
2017-06-15 08:36:45 +0200

    back-end transaction ????

--- common/models/customer.js
+++ common/models/customer.js
@@ -2,16 +2,42 @@
 
 module.exports = function(Customer) {
 
+    Customer.insert = function(data, cb) {
+        // ellenőrzéseket betenni
+        var customerInstance;
+        var rightInstance;
+
+        Customer.beginTransaction('READ COMMITTED', function(err, tx) {
+            Customer.create({email: data.email, password: data.password, username: data.username}, {transaction: tx})
+            .then(function(response) {
+                if(response) {
+                    tx.commit(function(err){});
+                    return response;
+//                    return CustomerRight.create({principalType: CustomerRight.USER, principalId: instance.id, roleId: data.rightId}, {transaction: tx});
+                } else {
+                    throw ({message: "Customer.create unsuccessfull", status: 400});
+                }
+            })
+/*            .then(function(instance) {
+                if(instance) {
+                    tx.commit(function(err){});
+                } else {
+                    throw ({message: "CustomerRight.create unsuccessfull", status: 400});                    
+                }
+            })
+*/            .catch(function(err) {
+                tx.rollback(function(err){});
+            });
+        });
+    }
+
     Customer.remoteMethod('insert', 
     {
-        description: 'Save a new customer and authirization (transaction).',
+        description: 'Save a new customer and authorization (transaction).',
+        http: {path: '/insert', verb: 'post'},
         accepts: [
-          {arg: 'email', type: 'string', required: true},
-          {arg: 'password', type: 'string', required: true},
-          {arg: 'username', type: 'string', required: false},
-          {arg: 'right', type: 'object', required: true},
+            {arg: 'data', type: 'object', http: { source: 'body' }}
         ],
-        returns: {arg: 'insert', type: 'object'},
-        http: {path: '/insert', verb: 'post'}
+        returns: {arg: 'insert', type: 'Customer', root: true}
     });
 };
